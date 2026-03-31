370694ae6c7457303680192cd98df057bd754571
shylit <trisvetovrichard@gmail.com>
2026-03-31 11:31:12 +0300

    Описывает таблицу в базе данных

---
+++ todolistapp/data/local/TaskEntity.kt
@@ -0,0 +1,13 @@
+package com.example.todolistapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val isCompleted: Boolean
+)
